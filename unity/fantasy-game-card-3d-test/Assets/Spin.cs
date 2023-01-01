6eb9c6a8cf5d1c0aa19aade67268f048dcc7e17f
Todd Kushner <kushnertodd@gmail.com>
2023-01-01 12:12:32 -0700

    removed fantasy-card-game-3

---
+++ unity/fantasy-game-card-3d-test/Assets/Spin.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    public float speed = 3.0f;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, speed, 0);
+    }
+}
